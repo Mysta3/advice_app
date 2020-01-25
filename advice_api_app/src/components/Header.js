3b83346b658e2735f93a2b29bb6aac83605cb13e
Mysta3 <myshawnestallings@gmail.com>
2020-01-24 18:45:52 -0800

    fixing security vulnerability

--- advice_api_app/src/components/Header.js
+++ advice_api_app/src/components/Header.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import '../index.css';
 
- function Header() {
+function Header() {
   return (
     <div>
       <h1>Header Title</h1>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
