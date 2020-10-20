f901f7ce03129d33616dee231f42c9ea3a182ae9
Mysta3 <myshawnestallings@gmail.com>
2020-10-20 09:25:36 -0700

    prep to redploy

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   const generateAdvice = () => {
     const rootUrl = 'https://api.adviceslip.com/advice'; // store base fetch url in a variable
     Axios.get(rootUrl).then(response => {
-      setAdvice(response.data.slip); //set data to getAdvice variable
+      setAdvice(response.data.slip); //set data to getAdvice variable | userfriendlyname
     });
   };
 
